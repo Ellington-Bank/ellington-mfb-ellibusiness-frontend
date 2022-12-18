5a5daabab5f31480a54d32dd8ef154637f308e82
Mykel-Eze <michaeleze1997@gmail.com>
2022-12-18 22:23:15 +0100

    Other pages

--- src/router/index.js
+++ src/router/index.js
@@ -13,44 +13,44 @@ import Home from '../views/dashboard-pages/Home.vue'
 import PersonalInfo from '../views/dashboard-pages/Personal-info.vue'
 import BankInfo from '../views/dashboard-pages/Bank-info.vue'
 import BankDetails from '../views/dashboard-pages/BankDetails.vue'
-// import ProcessingRequest from '../views/dashboard-pages/ProcessingRequest.vue'
-// import RequestDenied from '../views/dashboard-pages/RequestDenied.vue'
-// import Loans from '../views/dashboard-pages/Loans.vue'
-// import Payroll from '../views/dashboard-pages/Payroll.vue'
-// import RolesSettings from '../views/dashboard-pages/RolesSettings.vue'
+import ProcessingRequest from '../views/dashboard-pages/ProcessingRequest.vue'
+import RequestDenied from '../views/dashboard-pages/RequestDenied.vue'
+import Loans from '../views/dashboard-pages/Loans.vue'
+import Payroll from '../views/dashboard-pages/Payroll.vue'
+import RolesSettings from '../views/dashboard-pages/RolesSettings.vue'
 
 //***Transfer Pages***//
-// import Transfer from '../views/transfer-pages/Transfer.vue'
-// import BankTransfer from '../views/transfer-pages/BankTransfer.vue'
-// import AiropayTransfer from '../views/transfer-pages/AiropayTransfer.vue'
+import Transfer from '../views/transfer-pages/Transfer.vue'
+import BankTransfer from '../views/transfer-pages/BankTransfer.vue'
+import AiropayTransfer from '../views/transfer-pages/AiropayTransfer.vue'
 
 //***Collections Pages***//
-// import Collections from '../views/collections-pages/Collections.vue'
-// import BankTransferCollection from '../views/collections-pages/BankTransferCollection.vue'
-// import UssdCollection from '../views/collections-pages/UssdCollection.vue'
-// import PosCollection from '../views/collections-pages/PosCollection.vue'
-// import QrcodeCollection from '../views/collections-pages/QrcodeCollection.vue'
+import Collections from '../views/collections-pages/Collections.vue'
+import BankTransferCollection from '../views/collections-pages/BankTransferCollection.vue'
+import UssdCollection from '../views/collections-pages/UssdCollection.vue'
+import PosCollection from '../views/collections-pages/PosCollection.vue'
+import QrcodeCollection from '../views/collections-pages/QrcodeCollection.vue'
 
 //***Wallet Pages***//
-// import Wallet from '../views/wallet-pages/Wallet.vue'
-// import RequestCard from '../views/wallet-pages/RequestCard.vue'
-// import RequestCardSent from '../views/wallet-pages/RequestCardSent.vue'
+import Wallet from '../views/wallet-pages/Wallet.vue'
+import RequestCard from '../views/wallet-pages/RequestCard.vue'
+import RequestCardSent from '../views/wallet-pages/RequestCardSent.vue'
 
 //***Invoice Pages***//
-// import Invoice from '../views/invoice-pages/Invoice.vue'
-// import NewInvoice from '../views/invoice-pages/NewInvoice.vue'
-// import Invoice2 from '../views/invoice-pages/Invoice2.vue'
+import Invoice from '../views/invoice-pages/Invoice.vue'
+import NewInvoice from '../views/invoice-pages/NewInvoice.vue'
+import Invoice2 from '../views/invoice-pages/Invoice2.vue'
 
 //***Transactions Pages***//
 import Transactions from '../views/transactions-pages/Transactions.vue'
 import Transactions2 from '../views/transactions-pages/Transactions2.vue'
 
 //***Savings-and-Invest Pages***//
-// import SaveInvest from '../views/save-invest-pages/SaveInvest.vue'
-// import SaveInvest2 from '../views/save-invest-pages/SaveInvest2.vue'
+import SaveInvest from '../views/save-invest-pages/SaveInvest.vue'
+import SaveInvest2 from '../views/save-invest-pages/SaveInvest2.vue'
 
 //***More Dashboard Pages***//
-// import Support from '../views/Support.vue'
+import Support from '../views/Support.vue'
 
 Vue.use(VueRouter)
 
@@ -110,117 +110,117 @@ const routes = [
     name: 'Transactions2',
     component: Transactions2
   },
-  // {
-  //   path: '/processing-request',
-  //   name: 'ProcessingRequest',
-  //   component: ProcessingRequest
-  // },
-  // {
-  //   path: '/request-denied',
-  //   name: 'RequestDenied',
-  //   component: RequestDenied
-  // },
-  // {
-  //   path: '/loans',
-  //   name: 'Loans',
-  //   component: Loans
-  // },
-  // {
-  //   path: '/transfer',
-  //   name: 'Transfer',
-  //   component: Transfer
-  // },
-  // {
-  //   path: '/bank-transfer',
-  //   name: 'BankTransfer',
-  //   component: BankTransfer
-  // },
-  // {
-  //   path: '/airopay-transfer',
-  //   name: 'AiropayTransfer',
-  //   component: AiropayTransfer
-  // },
-  // {
-  //   path: '/collections',
-  //   name: 'Collections',
-  //   component: Collections
-  // },
-  // {
-  //   path: '/bank-transfer-collection',
-  //   name: 'BankTransferCollection',
-  //   component: BankTransferCollection
-  // },
-  // {
-  //   path: '/ussd-collection',
-  //   name: 'UssdCollection',
-  //   component: UssdCollection
-  // },
-  // {
-  //   path: '/pos-collection',
-  //   name: 'PosCollection',
-  //   component: PosCollection
-  // },
-  // {
-  //   path: '/qrcode-collection',
-  //   name: 'QrcodeCollection',
-  //   component: QrcodeCollection
-  // },
-  // {
-  //   path: '/wallet',
-  //   name: 'Wallet',
-  //   component: Wallet
-  // },
-  // {
-  //   path: '/request-card',
-  //   name: 'RequestCard',
-  //   component: RequestCard
-  // },
-  // {
-  //   path: '/request-card-sent',
-  //   name: 'RequestCardSent',
-  //   component: RequestCardSent
-  // },
-  // {
-  //   path: '/payroll',
-  //   name: 'Payroll',
-  //   component: Payroll
-  // },
-  // {
-  //   path: '/invoice',
-  //   name: 'Invoice',
-  //   component: Invoice
-  // },
-  // {
-  //   path: '/new-invoice',
-  //   name: 'NewInvoice',
-  //   component: NewInvoice
-  // },
-  // {
-  //   path: '/invoice2',
-  //   name: 'Invoice2',
-  //   component: Invoice2
-  // },
+  {
+    path: '/processing-request',
+    name: 'ProcessingRequest',
+    component: ProcessingRequest
+  },
+  {
+    path: '/request-denied',
+    name: 'RequestDenied',
+    component: RequestDenied
+  },
+  {
+    path: '/loans',
+    name: 'Loans',
+    component: Loans
+  },
+  {
+    path: '/transfer',
+    name: 'Transfer',
+    component: Transfer
+  },
+  {
+    path: '/bank-transfer',
+    name: 'BankTransfer',
+    component: BankTransfer
+  },
+  {
+    path: '/airopay-transfer',
+    name: 'AiropayTransfer',
+    component: AiropayTransfer
+  },
+  {
+    path: '/collections',
+    name: 'Collections',
+    component: Collections
+  },
+  {
+    path: '/bank-transfer-collection',
+    name: 'BankTransferCollection',
+    component: BankTransferCollection
+  },
+  {
+    path: '/ussd-collection',
+    name: 'UssdCollection',
+    component: UssdCollection
+  },
+  {
+    path: '/pos-collection',
+    name: 'PosCollection',
+    component: PosCollection
+  },
+  {
+    path: '/qrcode-collection',
+    name: 'QrcodeCollection',
+    component: QrcodeCollection
+  },
+  {
+    path: '/wallet',
+    name: 'Wallet',
+    component: Wallet
+  },
+  {
+    path: '/request-card',
+    name: 'RequestCard',
+    component: RequestCard
+  },
+  {
+    path: '/request-card-sent',
+    name: 'RequestCardSent',
+    component: RequestCardSent
+  },
+  {
+    path: '/payroll',
+    name: 'Payroll',
+    component: Payroll
+  },
+  {
+    path: '/invoice',
+    name: 'Invoice',
+    component: Invoice
+  },
+  {
+    path: '/new-invoice',
+    name: 'NewInvoice',
+    component: NewInvoice
+  },
+  {
+    path: '/invoice2',
+    name: 'Invoice2',
+    component: Invoice2
+  },
  
-  // {
-  //   path: '/save-and-invest',
-  //   name: 'SaveInvest',
-  //   component: SaveInvest
-  // },
-  // {
-  //   path: '/save-and-invest2',
-  //   name: 'SaveInvest2',
-  //   component: SaveInvest2
-  // },
-  // {
-  //   path: '/roles-and-settings',
-  //   name: 'RolesSettings',
-  //   component: RolesSettings
-  // },
-  // {
-  //   path: '/support',
-  //   name: 'Support',
-  //   component: Support
-  // }
+  {
+    path: '/save-and-invest',
+    name: 'SaveInvest',
+    component: SaveInvest
+  },
+  {
+    path: '/save-and-invest2',
+    name: 'SaveInvest2',
+    component: SaveInvest2
+  },
+  {
+    path: '/roles-and-settings',
+    name: 'RolesSettings',
+    component: RolesSettings
+  },
+  {
+    path: '/support',
+    name: 'Support',
+    component: Support
+  }
   // {
   //   path: '/about',
   //   name: 'About',
